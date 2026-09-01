187e5a2e9155a22f2a9492e11cb54177f1a8a2c1
jangjunseo518-collab <wkdwnstj00@naver.com>
2026-09-01 11:50:34 +0900

    초기 설정 완.

---
+++ src/main/java/com/sprintlogpractice/sprintlog_boot/SprintlogBootApplication.java
@@ -0,0 +1,13 @@
+package com.sprintlogpractice.sprintlog_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintlogBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintlogBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprintlogpractice/sprintlog_boot/SprintlogBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintlogpractice.sprintlog_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintlogBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
